25c732b7a9499ecba9df0d92f765b24509b39940
aivars17 <30680298+aivars17@users.noreply.github.com>
2017-08-25 17:04:56 +0300

    add font JS
    veikiantis javascript

--- script.js
+++ script.js
@@ -1,37 +1,29 @@
-var vieta = [];
 var skaicius;
 function go() {
 	clear();
 	skaicius = Math.floor(Math.random() * 4) + 1;
-	
 	document.getElementById(skaicius).style.backgroundColor = "red";
-	//document.getElementById('cia').innerHTML = vieta;
 }
 function back() {
 	clear();
-	//check_gameover();
-	if (skaicius <= 1) {
-		document.getElementById(4).style.backgroundColor = "red";
-		back();
+	if (!skaicius) {
+		return;
 	}	else  if (skaicius > 1) {
 		skaicius--;
 	}	else {
-		skaicius++;
-	}
+		skaicius = 4;
+	}	
 	document.getElementById(skaicius).style.backgroundColor = "red";
-	
 }
 function next() {
 	clear();
-	//vieta.push(skaicius);
-	if (skaicius >= 4) {
-		document.getElementById(1).style.backgroundColor = "red";
-		next();
+	if (!skaicius) {
+		return;
 	}	else if (skaicius < 4) {
 		skaicius++;
-	}	else {
-		skaicius--;
-	}
+	}	else {	
+		skaicius = 1;
+	} 
 	document.getElementById(skaicius).style.backgroundColor = "red";
 }
 function clear() {
@@ -39,8 +31,3 @@ function clear() {
 		document.getElementById(i).style.backgroundColor = "black";
 	}
 }
-//function check_gameover() {
-//	if (var b = 1; b = skaicius.length; b++) {
-//		document.getElementById(b).style.backgroundColor = "red";
-	//} 
-//}
